69627fa48f784ac0f03b01631bb96d9365aa3f13
balaji2210 <63396862+balaji2210@users.noreply.github.com>
2022-03-14 11:59:59 +0530

    removed redundent code

--- app.js
+++ app.js
@@ -19,14 +19,6 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("frontend/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res, sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
-
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", postRoutes);
 
